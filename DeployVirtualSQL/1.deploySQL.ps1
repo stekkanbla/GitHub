d4c3533f2022079122bb997e73b57cce1a23b3dd
TorO <toreistad@gmail.com>
2012-05-28 23:19:47 +0200

    1.11

---
+++ DeployVirtualSQL/1.deploySQL.ps1
@@ -0,0 +1,45 @@
+﻿
+
+### Legger til VMware.VimAutomation.Core snapin for tilgang på PowerCLI cmdlets ###
+Add-PSSnapin VMware.VimAutomation.Core
+
+### Henter loggin fra cred.xml for automatisk innloggin på vc-fag.studvir.aitel.hist. Passord hashed ###
+### Før du benytter denne metoden må du kjøre et script som lager XML-fila. Se under ###
+### New-VICredentialStoreItem -Host vc-fag.studvir.aitel.hist.no -User Administrator -Password "superduperpassword" -File "C:\whateverfolder\whatevername.xml" ###
+### Etter at du har kjørt script overfor vil det lages en XML-fil med brukernavn og hashed passord. Slett deretter scriptet med passord i klartekst ###
+### Brukes ikke for ESX senere i scriptet siden host må være med og dette vet en ikke før lengre ut i scriptet, kan ordnes med if ###
+
+### Kobler til vCenter ###
+$HostCred = $Host.UI.PromptForCredential("Please enter credentials", "Enter ESX host credentials for $ESXHost", "toroveorcli", "")
+Connect-VIServer -Server vc-fag.studvir.aitel.hist.no -Credential $HostCred
+#Get-Template -Name SharePointReady
+#Get-OSCustomizationSpec -id Bachelor47E.Template.Win2k8R2x64
+#New-vm -Name "MinVM" -VMHost "blad-06.virnett.aitel.hist.no" -ResourcePool "BACHELOR" -Location "47E" -DiskStorageFormat Thin -Datastore "QNAP-NFS"  -Template "W2k8R2.x64.template.47E"
+$n = 1
+$numberofmachines = 2
+while ($n -le $numberofmachines) {
+$name = "SQL$n"
+Write-host $name
+$Vmhost = "blad-06.virnett.aitel.hist.no"
+Write-host $vmhost
+$ResourcePool = "BACHELOR"
+Write-host $ResourcePool
+$location = "47E"
+Write-host $location
+$datastore = "QNAP-NFS"
+Write-host $datastore
+$osspec = "Bachelor47E.SQL2012Spec"
+Write-host $osspec
+$template = "sql2012Ready"
+Write-host $template
+
+### Kjører PowerCLI cmd for oppretting av virtuel maskin ### 
+ New-VM -Name $name -VMHost $Vmhost  -ResourcePool $ResourcePool -Location $location -Datastore $datastore  -Template SharePointReady -RunAsync
+#-OSCustomizationSpec $osspec
+#Start-Sleep -Seconds 5
+### Starter nylig opprettet VM ###
+Get-VM $name | start-vm 
+$n++
+#Start-Sleep -Seconds 5
+}
+
---
+++ DeployVirtualSQL/2.settIpSQLjoinDomain.ps1
@@ -0,0 +1,88 @@
+﻿## bortimot en kopi av 2.setSPip. så se der for dokumentasjon
+
+Function Set-WinVMIP ($VM, $HC, $GC, $IP, $SNM, $GW, $SanIP){
+ $netshIP = "c:\windows\system32\netsh.exe interface ip set address ""Local Area Connection"" static $IP $SNM $GW 1"
+ $netshIPSan = "c:\windows\system32\netsh.exe interface ip set address ""Local Area Connection 2"" static $SanIP $SNM"
+ Write-Host "Setting IP address for $VM..."
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType bat -ScriptText $netshIP -ErrorAction SilentlyContinue
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType bat -ScriptText $netshIPSan -ErrorAction SilentlyContinue
+ Write-Host "Setting IP address completed."
+ 
+  
+ $netshDNS1 = "c:\windows\system32\netsh.exe interface ip set dns ""Local Area Connection"" static 158.38.43.125"
+ $netshDNS2 = "c:\windows\system32\netsh.exe interface ip add dns ""Local Area Connection"" 158.38.49.10 index=2"
+ Write-Host "Setting DNS"
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType bat -ScriptText $netshDNS1 -ErrorAction SilentlyContinue
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType bat -ScriptText $netshDNS2 -ErrorAction SilentlyContinue
+ 
+ Write-Host "Setting DNS completed"
+ 
+ ##logger IP til fila "ipliste.txt"
+ $vmobj = Get-VM $vm
+ $IP = $vmobj.guest.ipAddress 
+ $vmobj.name+"    "+ $vmobj.guest.ipAddress | Out-File .\ipliste.txt -Append
+ }
+  
+ Function Join-Domain ($DCcred, $VM, $HC, $GC ) {
+ $domain = "47p.aitel.hist.no"
+ $shortDomain ="47p"
+ $user = $shortDomain+"\administrator"
+ $joinDomain = "add-computer -domainName $domain -credential $DCcred"
+ Write-Host "Joiner domenet: $domain"
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType PowerShell -ScriptText '&"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "Set-ExecutionPolicy bypass -force "' 
+ Invoke-VMScript -VM $VM -HostCredential $HC -GuestCredential $GC -ScriptType PowerShell -ScriptText '&"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "\\158.38.43.125\47p\ps\autoJoinDomain.ps1"' 
+}
+
+
+$ESXHost = get-vm Bach47e.dc2 | Get-VMHost
+
+### host credentials..eneste behov for interaksjon med bruker...promptes etter dette automatisk
+$HostCred = $Host.UI.PromptForCredential("Please enter credentials", "Enter ESX host credentials for $ESXHost", "toroveorcli", "")
+###
+
+###først oppgir vi passord for administrator og konverterer til securestring // fungerer ikke som ventet
+$PW = ConvertTo-SecureString "ZheShiWO3" -AsPlainText -Force
+#så bruker vi passordet for å opprette et objekt for creds
+#$GuestCred = $Host.UI.PromptForCredential("Please enter credentials", "Enter Guest credentials for $VM", "administrator", "")
+$GuestCred = New-Object System.Management.Automation.PSCredential ("administrator", $PW)
+###end
+	
+$dcpw = ConvertTo-SecureString "ZheShiWO3" -AsPlainText -Force
+##### DC cred (litt shaky i klartekst
+$DCcred = New-Object System.Management.Automation.PSCredential ("47p\administrator", $DCpw)
+
+### starter med maskin 1
+$n = 1
+# sett høyeste nummer her
+$nmax = 2
+###
+
+### IP calc
+# legger til $n på $IPstart (forutsetter at vi har en vedvarende range for alle maskinene
+# bruker her 158.38.57.
+# .226-228, så må trekke fra 1 (siden $n starter på 1)
+$IPStart = 225
+$SanIPstart = 240
+###
+
+
+while ($n -le $nmax)
+{
+	
+	$IPcurrent = $IPStart+$n
+	$VM = "SQL$n"
+	$ESXHost = get-vm $vm | Get-VMHost
+
+	$IP = "158.38.57.$IPcurrent"
+	$SNM = "255.255.255.0"
+	$GW = "158.38.43.1"
+
+	$SanIPCurrent = $SanIPstart+$n
+	$SanIP = "172.16.101.$SanIPcurrent"
+### bruker samme nettmaske som $SNM
+
+#Set-WinVMIP $VM $HostCred $GuestCred $IP $SNM $GW $SanIP
+Join-Domain $DCcred $VM $HostCred $GuestCred
+#Get-VM "SQL$n" | Restart-VMGuest
+$n++
+} #end while
